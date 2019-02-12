972a59c88af199e9809e1ce2e5767fb8ca59a6b8
as3p <as3p.ramdhani@gmail.com>
2019-02-12 13:04:59 +0700

    Generate Serial Number

---
+++ GenerateSN/GenerateSN/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(311, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(78, 134)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Create"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(31, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Bulan                :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(31, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Tahun               :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(30, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 17)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Model              :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(30, 130)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(105, 17)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Range SN         :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(143, 17)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 17)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "-"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(143, 49)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 17)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "-"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(143, 92)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(10, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "-"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.TextBox1.Location = New System.Drawing.Point(146, 127)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(57, 25)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.TextBox2.Location = New System.Drawing.Point(228, 127)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(57, 25)
+        Me.TextBox2.TabIndex = 9
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(395, 21)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(139, 134)
+        Me.ListBox1.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(210, 136)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(10, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "-"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(563, 174)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Generate Serial Number"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ GenerateSN/GenerateSN/Form1.vb
@@ -0,0 +1,120 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        '====== awal set bulan ======
+        Dim bulan As String = ""
+        If Format(Now, "MM") = "01" Then
+            bulan = "01"
+        ElseIf Format(Now, "MM") = "02" Then
+            bulan = "02"
+        ElseIf Format(Now, "MM") = "03" Then
+            bulan = "03"
+        ElseIf Format(Now, "MM") = "04" Then
+            bulan = "04"
+        ElseIf Format(Now, "MM") = "05" Then
+            bulan = "05"
+        ElseIf Format(Now, "MM") = "06" Then
+            bulan = "06"
+        ElseIf Format(Now, "MM") = "07" Then
+            bulan = "07"
+        ElseIf Format(Now, "MM") = "08" Then
+            bulan = "08"
+        ElseIf Format(Now, "MM") = "09" Then
+            bulan = "09"
+        ElseIf Format(Now, "MM") = "10" Then
+            bulan = "A"
+        ElseIf Format(Now, "MM") = "11" Then
+            bulan = "B"
+        ElseIf Format(Now, "MM") = "12" Then
+            bulan = "C"
+        End If
+        '====== akhir set bulan ======
+
+        '====== awal set tahun ======
+        Dim tahun As String = ""
+        If Format(Now, "yyyy") = "2018" Then
+            tahun = "18"
+        ElseIf Format(Now, "yyyy") = "2019" Then
+            tahun = "BB"
+        ElseIf Format(Now, "yyyy") = "2020" Then
+            tahun = "CC"
+        ElseIf Format(Now, "yyyy") = "2021" Then
+            tahun = "DD"
+        ElseIf Format(Now, "yyyy") = "2022" Then
+            tahun = "EE"
+        End If
+        '====== akhir set tahun ======
+
+        '====== awal set model ======
+        Dim model As String = ""
+        If Label8.Text = "Model 1" Then
+            model = "M1"
+        ElseIf Label8.Text = "Model 2" Then
+            model = "M2"
+        ElseIf Label8.Text = "Model 3" Then
+            model = "M3"
+        ElseIf Label8.Text = "Model 4" Then
+            model = "M4"
+        ElseIf Label8.Text = "Model 5" Then
+            model = "M5"
+        End If
+        '====== akhir set tahun ======
+
+        '====== set nomor ======
+        Dim i As Integer
+        Dim j As Integer
+        ListBox1.Items.Clear()
+        i = 1
+        j = i + 1
+        Dim k As String = ""
+
+        'Looping serial number
+        For j = TextBox1.Text To TextBox2.Text
+            If j <= 9 Then
+                k = "AS" & tahun & bulan & "00000" & j & model
+            ElseIf j <= 99 Then
+                k = "AS" & tahun & bulan & "0000" & j & model
+            ElseIf j <= 999 Then
+                k = "AS" & tahun & bulan & "000" & j & model
+            ElseIf j <= 9999 Then
+                k = "AS" & tahun & bulan & "00" & j & model
+            ElseIf j <= 99999 Then
+                k = "AS" & tahun & bulan & "0" & j & model
+            ElseIf j > 999999 Then
+                MsgBox("Batas Nomor Generate Sampai 999999")
+                'stop looping
+                Exit Sub
+            Else
+                k = "AS" & tahun & bulan & j & model
+            End If
+            ListBox1.Items.Add(k.ToString)
+        Next (j)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label5.Text = Format(Now, "MMMM")
+        Label6.Text = Format(Now, "yyyy")
+        Label7.Text = "Model 1"
+    End Sub
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        'maskimal jumlah yang di input
+        TextBox1.MaxLength = 6
+        'hanya angka yang boleh di input
+        If Strings.InStr("0123456789" & Chr(8), e.KeyChar) = 0 Then
+            e.KeyChar = Chr(0)
+        End If
+    End Sub
+
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox2.KeyPress
+        'maskimal jumlah yang di input
+        TextBox2.MaxLength = 6
+        'hanya angka yang boleh di input
+        If Strings.InStr("0123456789" & Chr(8), e.KeyChar) = 0 Then
+            e.KeyChar = Chr(0)
+        End If
+    End Sub
+
+  
+End Class
